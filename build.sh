71ce565d766e45e521520055059d8f6d2bf04251
juguofeng <jgfntu@gmail.com>
2013-05-18 22:07:35 +0800

    [FIXBUG bug#0002*]重新修复NEON问题，只需采用sh build.sh即可；修改ffmpeg库为0.10.7，稍后给出编译方式和源码及补丁

--- build.sh
+++ build.sh
@@ -1,63 +1,15 @@
 #!/bin/sh
 
-##@{	add by juguofeng	2013-04-18
+rm -rf libs/armeabi*
+ruby pre-build.rb
+ndk-build $@ || exit 1
+rm -rf assets/lib
+ruby post-build.rb
+rm -rf bin
+ant release
+test -f bin/faplayer-unsigned.apk || exit 1
+java -jar signapk.jar testkey.x509.pem testkey.pk8 bin/faplayer-unsigned.apk bin/faplayer.apk
+mv bin/faplayer.apk bin/faplayer-not-aligned.apk
+zipalign -f 4 bin/faplayer-not-aligned.apk bin/faplayer.apk
+echo "Done"
 
-COLOR_RS="\033[31;49;1m"
-COLOR_BS="\033[34;49;1m"
-COLOR_ED="\033[39;49;0m"
-
-DEFAULT="sh fa_build.sh # <-- this will build for cortex-a8 with neon by default"
-ARMEABI="sh fa_build.sh ABI=armeabi # <-- this will build for arm1136j-s with softfp"
-ARMEABI_V7A="sh fa_build.sh ABI=armeabi-v7a FPU=vfpv3-d16 TUNE=cortex-a9 # <-- Xoom"
-
-usage() {
-	echo "======================================================================"
-	echo $COLOR_RS"please input the correct <os-option number>"$COLOR_ED
-	echo ""
-	echo "Usage: sh $0 <os-option number>"
-	echo "=========>"$COLOR_BS"[1 -> DEFAULT]"$COLOR_ED "or" $COLOR_BS"[2 -> ARMEABI]"$COLOR_ED "or" $COLOR_BS"[3 -> ARMEABI_V7A]"$COLOR_ED
-	echo ""
-	echo "meaning:"
-	echo "["$COLOR_BS"  DEFAULT  "$COLOR_ED"]"$DEFAULT
-	echo "["$COLOR_BS"  ARMEABI  "$COLOR_ED"]"$ARMEABI
-	echo "["$COLOR_BS"ARMEABI_V7A"$COLOR_ED"]"$ARMEABI_V7A
-	echo "======================================================================"
-	exit 1
-}
-
-if [ $# -ne 1 ]
-then
-	usage $*
-fi
-
-if [ "DEFAULT" = "$1" -o "1" = "$1" ]
-then
-	echo ""
-	echo $COLOR_BS"======>compiling for Android ["$COLOR_BS"  DEFAULT  ]..."$COLOR_ED
-	echo ""
-	sh fa_build.sh
-	exit
-fi
-	
-if [ "ARMEABI" = "$1" -o "2" = "$1" ]
-then
-	echo ""
-	echo $COLOR_BS"======>compiling for Android ["$COLOR_BS"  ARMEABI  ]..."$COLOR_ED
-	echo ""
-	sh fa_build.sh ABI=armeabi
-	exit
-fi
-
-if [ "ARMEABI_V7A" = "$1" -o "3" = "$1" ]
-then
-	echo ""
-	echo $COLOR_BS"======>compiling for Android ["$COLOR_BS"ARMEABI_V7A]..."$COLOR_ED
-	echo ""
-	sh fa_build.sh ABI=armeabi-v7a FPU=vfpv3-d16 TUNE=cortex-a9
-	exit
-fi
-	
-usage $*
-
-
-##@}	end		###
