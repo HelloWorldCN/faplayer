b0718a536c719aec635fe93ecda2cec9dd8ce184
juguofeng <ju_guofeng@hoperun.com>
2013-05-21 14:24:49 +0800

    [FIXBUG bug#0008][ffmpeg beta]add an applehttp.patch to ffmpeg-0.8.14 to fix bug#0008

--- jni/cross-compile/compile.sh
+++ jni/cross-compile/compile.sh
@@ -9,13 +9,16 @@ if [ ! -d "android" ]; then
 	mkdir android
 fi
 
-if [ ! -d "android/ffmpeg" ]; then
-	tar xvf tarballs/ffmpeg-0.8.14.tar.bz2
-	mv ffmpeg-0.8.14/ android/ffmpeg
-	cd android/ffmpeg
+if [ ! -d "android/ffmpeg-0.8.14" ]; then
+	tar xf tarballs/ffmpeg-0.8.14.tar.bz2
+	mv ffmpeg-0.8.14 android/
+	cp patch/ffmpeg/applehttp.patch android/ffmpeg-0.8.14
+	cd android/ffmpeg-0.8.14
+	patch -p1 < applehttp.patch
+	rm applehttp.patch
 	./configure  --prefix=../../../arm-linux-androideabi --enable-cross-compile --target-os=linux --arch=arm --cpu=cortex-a8 --enable-neon --cross-prefix=arm-linux-androideabi-
 else
-	cd android/ffmpeg
+	cd android/ffmpeg-0.8.14
 fi
 
 make -j4
