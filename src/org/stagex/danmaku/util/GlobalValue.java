7ba2771f22aa853b8502851fbddef03fc2494e45
juguofeng <jgfntu@gmail.com>
2013-08-09 00:09:11 +0800

    [hls-v1.3.1]发布可可电视1.3.1版本

--- src/org/stagex/danmaku/util/GlobalValue.java
+++ src/org/stagex/danmaku/util/GlobalValue.java
@@ -3,7 +3,7 @@ package org.stagex.danmaku.util;
 public class GlobalValue {
 	
 	// 软件自带的节目列表的版本
-	public static final String tvlistVersion = "20130805";
+	public static final String tvlistVersion = "20130809";
 	
 	// 数据库的版本(考虑到数据库的特殊性，每次更新需要备份在此
 	public static final int dataBaseVerion = 4;				/* modify 2013-07-28 */
